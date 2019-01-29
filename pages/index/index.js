e5faaf973e166d6790378d4058e526c9580e9c56
unknown <lizhichange>
2019-01-29 18:39:25 +0800

    '1'

--- pages/index/index.js
+++ pages/index/index.js
@@ -48,7 +48,14 @@ Page({
   },
   renderBavbar: function (e) {
     wx.navigateTo({
-      url: '../navbar/navbar'
+      url: '../radio/index'
+    });
+  },
+
+
+  renderRadiobar: function (e) {
+    wx.navigateTo({
+      url: '../modal/index'
     });
   },
 
@@ -67,5 +74,29 @@ Page({
       userInfo: e.detail.userInfo,
       hasUserInfo: true
     })
+  },
+
+
+
+  onReady: function () {
+    //获得popup组件
+    this.popup = this.selectComponent("#popup");
+  },
+
+  showPopup() {
+    this.popup.showPopup();
+  },
+
+  //取消事件
+  _error() {
+    console.log('你点击了取消');
+    this.popup.hidePopup();
+  },
+  //确认事件
+  _success() {
+    console.log('你点击了确定');
+    this.popup.hidePopup();
   }
+
+
 });
